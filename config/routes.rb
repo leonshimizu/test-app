642eb883b2e4ecd5a3957ea6db72cb90a61f4e61
“leonshimizu” <lmshimizu@gmail.com>
2021-10-15 13:54:10 -0700

    created scaffold and migrated it

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :friends
   root 'pages#index'
   get 'about_page' => 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
